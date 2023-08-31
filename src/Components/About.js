150c2d3fb5456c54c4bc7f8654176532718bffa3
Anshjatana <anshjatana1111@gmail.com>
2023-08-31 14:25:28 +0530

    Added About section

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,11 +18,11 @@ const About = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-8">
       <h6 className="text-white text-[15px] font-semibold uppercase text-center  ">
-        {" "}
+        
         Get to know
       </h6>
       <h3 className="text-white text-[30px] font-semibold uppercase text-center ">
-        About Me{" "}
+        About Me
       </h3>
       <div className="flex  items-center justify-evenly ">
         <img src={memoji} alt="memoji" className="h-[400px]" />
